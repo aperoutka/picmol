41a02a30b6d91626ca5437812db7808167af559e
aperoutka <allison.peroutka98@gmail.com>
2025-03-23 16:37:55 -0500

    setup

--- README.md
+++ README.md
@@ -1,17 +1,61 @@
+# picmol: Molecular Simulation Analysis and Phase Equilibria Calculations
 
-# PICMOL: Phase Instability Calculator for Molecular Design
+`picmol` is a `Python` library designed to streamline the analysis of molecular dynamics simulations, focusing on the application of Kirkwood-Buff (KB) theory for the calculation of activity coefficients, excess thermodynamic properties, and liquid-liquid equilibria (LLE).
 
-## Features
+.. image:: http://img.shields.io/pypi/v/picmol.svg?style=flat
+  :target: https://pypi.python.org/pypi/picmol
+  :alt: Version_status
+.. image:: http://img.shields.io/badge/license-MIT-blue.svg?style=flat
+  :target: https://github.com/aperoutka/picmol/blob/main/LICENSE
+  :alt: license
+.. image:: https://img.shields.io/pypi/pyversions/picmol.svg
+  :target: https://pypi.python.org/pypi/picmol
+  :alt: Supported_versions
 
-- calculates Kirkwood-Buff Integrals (KBI) from radial distribution functions
-- support for UNIFAC, UNIQUAC, NRTL, and FH thermodynamic models
-- phase diagram calculator for LLE applications
-- calculation of Io in small-angle X-ray scattering
-  
-## Environment Variables
+**Key Features:**
 
-To run this project, create an anaconda environment from environment.yml
+* **Kirkwood-Buff Integration:**
+    * Calculates interaction parameters from molecular dynamics simulations using Kirkwood-Buff theory.
+    * Fits these parameters to various thermodynamic models, including:
+        * Numerical models (e.g., quartic)
+        * UNIQUAC
+        * NRTL
+        * Flory-Huggins
+* **Liquid-Liquid Equilibria (LLE) Calculations:**
+    * Performs phase diagram calculations for LLE systems, leveraging the KB-derived interaction parameters.
+    * Generates phase diagrams for visual analysis.
+* **Small-Angle X-ray Scattering (SAXS) Support:**
+    * Includes functionality to calculate Io from SAXS data, providing valuable structural information.
+* **Visualization Tools:**
+    * Offers comprehensive visualization capabilities for both Kirkwood-Buff analysis results and LLE phase diagrams, aiding in data interpretation.
+
+**Workflow:**
+
+`picmol` provides a cohesive workflow that bridges the gap between molecular dynamics simulations and phase equilibria calculations. It enables users to:
+
+1.  **Analyze MD simulations:** Use Kirkwood-Buff theory to derive interaction parameters.
+2.  **Fit thermodynamic models:** Fit the derived parameters to suitable thermodynamic models.
+3.  **Calculate LLE:** Predict liquid-liquid equilibria and generate phase diagrams.
+4.  **Analyze SAXS data:** Derive Io values from SAXS data.
+5.  **Visualize results:** Generate informative plots for analysis and presentation.
+
+**Applications:**
+
+`picmol` is a valuable tool for researchers and engineers working in fields such as:
+
+* Chemical engineering
+* Materials science
+* Pharmaceutical development
+* Polymer science
+
+**Getting Started:**
+
+Creating an anaconda environment with dependencies.
 
 ```python
 conda env create -f environment.yml
 ```
+
+pip install picmol
+
+
--- src/picmol/kbi_analysis.py
+++ src/picmol/kbi_analysis.py
@@ -13,12 +13,12 @@ def main():
 
   parser.add_argument('--prj_path', default=os.getcwd(), help='Path to project directory containing system folders (default: current working directory)')
   parser.add_argument('--pure_component_path', default=os.path.abspath(os.path.join(os.getcwd(), os.pardir, 'pure_components')), help='Path to pure component directory (default: parent_dir/pure_components)')
-  parser.add_argument('--rdf_dir', default='rdf_files', help='Name for rdf directory in each system (default: rdf_files)')
-  parser.add_argument('--kbi_dir', default='kbi_analysis', help='Name for directory for kbi analysis (default: kbi_analysis)')
-  parser.add_argument('--kbi_method', default='adj', choices=['raw', 'adj', 'kgv'], help='KBI method name (default: adj)')
+  parser.add_argument('--rdf_dir', type=str, default='rdf_files', help='Name for rdf directory in each system (default: rdf_files)')
+  parser.add_argument('--kbi_dir', type=str, default='kbi_analysis', help='Name for directory for kbi analysis (default: kbi_analysis)')
+  parser.add_argument('--kbi_method', type=str, default='adj', choices=['raw', 'adj', 'kgv'], help='KBI method name (default: adj)')
   parser.add_argument('--start_time', type=float, default=100, help='Time in ns to start averaging (default: 100)')
   parser.add_argument('--end_time', type=float, default=None, help='Time in ns to end averaging (default: end of trajectory)')
-  parser.add_argument('--thermo_model', default='quartic', choices=['quartic', 'uniquac', 'unifac', 'nrtl', 'fh'], help='Thermodynamic model name for LLE calculation (default: quartic)')
+  parser.add_argument('--thermo_model', type=str, default='quartic', choices=['quartic', 'uniquac', 'unifac', 'nrtl', 'fh'], help='Thermodynamic model name for LLE calculation (default: quartic)')
   parser.add_argument('--Tmin', type=float, default=150, help='Minimum Temp (K) for temperature sacling (default: 150)')
   parser.add_argument('--Tmax', type=float, default=400, help='Maximum Temp (K) for temperature scaling (default: 400)')
   parser.add_argument('--dT', type=float, default=1, help='Temperature step (K) for temperature scaling (default: 1) ')
@@ -27,20 +27,27 @@ def main():
   args = parser.parse_args()
 
   # initialize kbi object
-  kbi_obj = KBI(prj_path=args.prj_path, pure_component_path=args.pure_component_path, rdf_dir=args.rdf_dir, kbi_method=args.kbi_method, avg_start_time=args.start_time, avg_end_time=args.end_time, kbi_fig_dirname=args.kbi_fig_dirname)
+  print('intializing kbi object')
+  kbi_obj = KBI(prj_path=args.prj_path, pure_component_path=args.pure_component_path, rdf_dir=args.rdf_dir, kbi_method=args.kbi_method, avg_start_time=args.start_time, avg_end_time=args.end_time, kbi_fig_dirname=args.kbi_dir)
 
   # run kbi analysis
+  print('computing kbis')
   kbi_obj.kbi_analysis()
 
   # create figures
+  print('creating figures')
   kbi_plotter = KBIPlotter(kbi_obj)
   kbi_plotter.make_figures()
 
   # create thermodynamic model
+  print('initializing thermodynamic model')
   tmodel = ThermoModel(model_name=args.thermo_model, KBIModel=kbi_obj, dT=args.dT, Tmin=args.Tmin, Tmax=args.Tmax)
+
+  print('performing temperature scaling')
   tmodel.temperature_scaling()
 
   # create phase diagram figures
+  print('creating figures')
   tmodel_plotter = PhaseDiagramPlotter(tmodel)
   tmodel_plotter.make_figures()
 
